cfa7ca3746f45b2768b9e3e1aa7776b6d32028c7
Matteo Tullo <matteotullo@microsoft.com>
2025-03-18 17:05:03 -0700

    WIP

--- battery-service/src/charger/mod.rs
+++ battery-service/src/charger/mod.rs
@@ -2,23 +2,26 @@ use core::cell::RefCell;
 
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use embassy_sync::channel::Channel;
+use embassy_sync::mutex::Mutex;
 use embedded_services::error;
 
-use crate::BatteryMsgs;
+use crate::{BatteryMsgTrait, BatteryMsgs};
 
 pub enum ChargerError {
     Bus,
 }
 
-pub struct Charger<SmartCharger: embedded_batteries_async::charger::Charger> {
+pub struct Charger<SmartCharger: embedded_batteries_async::charger::Charger, CustomMsgs: BatteryMsgTrait> {
     device: RefCell<SmartCharger>,
-    pub(crate) rx: Channel<NoopRawMutex, crate::BatteryMsgs, 1>,
+    pub(crate) rx: Channel<NoopRawMutex, crate::BatteryMsgs<CustomMsgs>, 1>,
 
     // Should size of channel be increased as a flurry of messages will need to be sent with broadcasts?
-    pub(crate) tx: Channel<NoopRawMutex, Result<crate::BatteryMsgs, ChargerError>, 1>,
+    pub(crate) tx: Channel<NoopRawMutex, Result<crate::BatteryMsgs<CustomMsgs>, ChargerError>, 1>,
 }
 
-impl<SmartCharger: embedded_batteries_async::charger::Charger> Charger<SmartCharger> {
+impl<SmartCharger: embedded_batteries_async::charger::Charger, CustomMsgs: BatteryMsgTrait>
+    Charger<SmartCharger, CustomMsgs>
+{
     pub fn new(smart_charger: SmartCharger) -> Self {
         Charger {
             device: RefCell::new(smart_charger),
@@ -52,7 +55,8 @@ impl<SmartCharger: embedded_batteries_async::charger::Charger> Charger<SmartChar
                         .map_err(|_| ChargerError::Bus);
                     self.tx.send(res).await;
                 }
-            },
+            }
+            BatteryMsgs::OemGeneric(msg),
             _ => error!("Unexpected message sent to charger"),
         }
     }
--- battery-service/src/lib.rs
+++ battery-service/src/lib.rs
@@ -18,12 +18,29 @@ pub enum OemMessage {
     ChargeCurrent(MilliAmps),
 }
 
+pub trait BatteryMsgTrait {
+    fn is_charger_msg(&self) -> bool;
+}
+
+impl BatteryMsgTrait for BatteryMessage {
+    fn is_charger_msg(&self) -> bool {
+        false
+    }
+}
+
+impl BatteryMsgTrait for OemMessage {
+    fn is_charger_msg(&self) -> bool {
+        true
+    }
+}
+
 /// Generic to hold OEM messages and standard ACPI messages
 /// Can add more as more services have messages
 #[derive(Copy, Clone, Debug)]
-pub enum BatteryMsgs {
+pub enum BatteryMsgs<T: BatteryMsgTrait> {
     Acpi(BatteryMessage),
     Oem(OemMessage),
+    OemGeneric(T),
 }
 
 /// Battery Service Errors
@@ -58,7 +75,7 @@ impl<
 
     /// Function to request a variable amount of battery messages.
     /// Intended to be called within a timer callback context, for example to get the voltage every 1 second.
-    pub async fn broadcast_dynamic_battery_msgs(&self, messages: &[BatteryMsgs]) {
+    pub async fn broadcast_dynamic_battery_msgs<T: BatteryMsgTrait>(&self, messages: &[BatteryMsgs<T>]) {
         for msg in messages {
             match msg {
                 BatteryMsgs::Acpi(BatteryMessage::CycleCount(_)) => self.fuel_gauge.rx.send(*msg).await,
@@ -69,13 +86,21 @@ impl<
                 BatteryMsgs::Acpi(BatteryMessage::RemainCap(_)) => self.fuel_gauge.rx.send(*msg).await,
                 BatteryMsgs::Acpi(BatteryMessage::PresentVolt(_)) => self.fuel_gauge.rx.send(*msg).await,
 
+                BatteryMsgs::OemGeneric(msg) => {
+                    if msg.is_charger_msg() {
+                        self.charger.rx.send(*msg).await
+                    } else {
+                        self.fuel_gauge.rx.send(*msg).await
+                    }
+                }
+
                 // More message support to be added.
                 _ => todo!(),
             }
         }
     }
 
-    fn handle_transport_msg(&self, msg: BatteryMsgs) -> Result<(), BatteryServiceErrors> {
+    fn handle_transport_msg<T: BatteryMsgTrait>(&self, msg: BatteryMsgs<T>) -> Result<(), BatteryServiceErrors> {
         match msg {
             BatteryMsgs::Acpi(msg) => match msg {
                 // Route to charger buffer or fuel gauge buffer
@@ -90,6 +115,7 @@ impl<
                     .map_err(|_| BatteryServiceErrors::BufferFull),
                 _ => todo!(),
             },
+            _ => unreachable!(),
         }
     }
 
@@ -136,14 +162,17 @@ impl<
     fn receive(&self, message: &comms::Message) {
         if let Some(msg) = message.data.get::<BatteryMessage>() {
             // Todo: Handle case where buffer is full.
-            if self.handle_transport_msg(BatteryMsgs::Acpi(*msg)).is_err() {
+            if self
+                .handle_transport_msg::<BatteryMessage>(BatteryMsgs::Acpi(*msg))
+                .is_err()
+            {
                 error!("Buffer full");
             }
         }
 
         if let Some(msg) = message.data.get::<OemMessage>() {
             // Todo: Handle case where buffer is full.
-            if self.handle_transport_msg(BatteryMsgs::Oem(*msg)).is_err() {
+            if self.handle_transport_msg::<OemMessage>(BatteryMsgs::Oem(*msg)).is_err() {
                 error!("Buffer full");
             }
         }
@@ -158,7 +187,7 @@ impl<
 /// - fuel_gauge_task()
 #[macro_export]
 macro_rules! create_battery_service {
-    ($charger:ident, $charger_bus:path, $fuel_gauge:ident, $fuel_gauge_bus:path) => {
+    ($charger:ident, $charger_bus:path, $fuel_gauge:ident, $fuel_gauge_bus:path, $custom_msgs:ident) => {
         use ::battery_service::{BatteryServiceErrors, Service};
         use ::embedded_services::{comms, error};
         static SERVICE: OnceLock<Service<$charger<$charger_bus>, $fuel_gauge<$fuel_gauge_bus>>> = OnceLock::new();
