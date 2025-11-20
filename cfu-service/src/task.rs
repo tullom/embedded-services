eab7e3864a74cda4eafcb04dbc034e2569259e35
Matteo Tullo <matteotullo@microsoft.com>
2025-11-20 13:44:24 -0800

    cfu: remove dependency on embassy executor

---
+++ cfu-service/src/task.rs
@@ -0,0 +1,21 @@
+use embassy_sync::once_lock::OnceLock;
+use embedded_services::{comms, error, info};
+
+use crate::CfuClient;
+
+pub async fn task() {
+    info!("Starting cfu client task");
+    static CLIENT: OnceLock<CfuClient> = OnceLock::new();
+    let cfuclient = CLIENT.get_or_init(|| CfuClient::create().expect("cfu client singleton already initialized"));
+
+    if comms::register_endpoint(cfuclient, &cfuclient.tp).await.is_err() {
+        error!("Failed to register cfu endpoint");
+        return;
+    }
+
+    loop {
+        if let Err(e) = cfuclient.process_request().await {
+            error!("Error processing request: {:?}", e);
+        }
+    }
+}
